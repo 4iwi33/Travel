5d52a08328e2143ebc4e236156f1fe395584fa9e
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-10-12 22:58:31 +0300

    updt

--- index.php
+++ index.php
@@ -6,20 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <title>Travel</title>
+    <title>index</title>
 </head>
 
 <body>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-        <h5 class="my-0 mr-md-auto font-weight-normal">Путешествия по Амерке</h5>
-        <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-dark" href="#">Главная</a>
-            <a class="p-2 text-dark" href="#">Регистрация</a>
-        </nav>
-        <a class="btn btn-outline-primary" href="#">Войти</a>
-    </div>
-
-
+    <?php require "header.php" ?>
     <div class="container mt-5">
         <div class="d-flex flex-wrap">
             <?php for ($i = 0; $i < 50; $i++) : ?>
@@ -30,8 +21,6 @@
                     <div class="card-body">
                         <img src="img/<?php echo ($i + 1) ?>.jpg" class="img-thumbnail">
                         <ul class="list-unstyled mt-3 mb-4">
-                            <li>20 users included</li>
-                            <li>10 GB of storage</li>
                         </ul>
                         <button type="button" class="btn btn-lg btn-block btn-primary">Посетить</button>
                     </div>
